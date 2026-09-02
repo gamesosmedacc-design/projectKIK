7990185d98ce7b57c222e478f418520b9e776c48
xenoo_ch <gamesosmedacc@gmai.com>
2026-09-02 12:54:35 +0800

    snapshot 020926 12:54 -r ibadah

--- KIK-webapp/frontend/js/login.js
+++ KIK-webapp/frontend/js/login.js
@@ -63,18 +63,20 @@ document.getElementById("login_link").addEventListener("click", (e)=> {
 
 document.getElementById("register_box").addEventListener("submit", async function (e) {
     e.preventDefault();
-
+    
     const create_username = document.getElementById("create_username").value;
     const create_password = document.getElementById("create_password").value;
     const Nis = document.getElementById("create_nis").value;
     const class_ = document.getElementById("class_").value;
     const email = document.getElementById("email").value;
     const phone_number = document.getElementById("number").value;
-    const role = document.getElementById("role").value;
+    const role = document.getElementById("role");
+    const role_value = role.value;
+    const class_box = getElementById("class_");
 
     const num_regex = /[0-9]/;
     const char_regex = /[a-zA-Z]/;
-
+    
     // username ==========================================================
     if (create_username === "") {
         alert("nama pengguna kamu belum ada");
@@ -85,7 +87,7 @@ document.getElementById("register_box").addEventListener("submit", async functio
         alert("username kamu terlalu panjang (tidak boleh lebih dari 50)");
         return;
     }
-
+    
     // password ==========================================================
     if (create_password === "") {        
         alert("password kamu belum ada");
@@ -101,12 +103,12 @@ document.getElementById("register_box").addEventListener("submit", async functio
         alert("kombinasi password terbaik adalah angka + huruf");
         return;
     }
-
+    
     if (!char_regex.test(create_password)) {
         alert("kombinasi password terbaik adalah angka + huruf");
         return;
     }
-
+    
     // nis ===============================================================
     if (Nis === "") {
         alert("Nis kamu belum ada");
@@ -117,16 +119,16 @@ document.getElementById("register_box").addEventListener("submit", async functio
         alert("kamu yakin itu nis kamu?");
         return;
     }
-
+    
     // class =============================================================
     if (class_ === "") {
         alert("kelas kamu belum ada");
         return;
     }
-
+    
     const index_class = class_.trim().split(/\s+/);
     const class_num_regex = /^(X|XI|XII|10|11|12)$/i;
-    const major_regex = /^(TP|TKJ|TKR|TKP|ALDP|ATPH|DPIB)$/
+    const major_regex = /^( TP|TKJ|TKR|TKP|ALDP|ATPH|DPIB)$/
 
     if (index_class.length < 2) {
         alert("formatnya harus <kelas> <jurusan>");
@@ -142,18 +144,18 @@ document.getElementById("register_box").addEventListener("submit", async functio
         alert("jurusan tidak valid gunakan (TP, TKJ, TKR, TKP, ALDP, ATPH, DPIB)");
         return;
     }
-
+    
     // email =============================================================
     if (email === "") {
         alert("email kamu belum ada");
         return;
     }
-
+    
     if (!email.endsWith("@gmail.com")) {
         alert("email harus diakhiri dengan @gmail.com");
         return;
     }
-
+    
     // phone number =======================================================
     const symbol_regex = /[^a-zA-Z0-9]/
     
@@ -161,25 +163,24 @@ document.getElementById("register_box").addEventListener("submit", async functio
         alert("nomor hp kamu tidak valid");
         return;
     }
-
+    
     if (char_regex.test(phone_number)) {
         alert("nomer hp tidak boleh ada hurufnya");
         return;
     }
-
+    
     if (symbol_regex.test(phone_number)) {
         alert("nomor hp tidak boleh mengandung simbol");
         return;
     }
-
+    
     // role ==============================================================
-    if (role === "teacher") {
-        alert("kamu adalah guru");
-        return;
-    }
-
-    if (role === "student") {
-        alert("kamu adalah murid");
-        return;
-    }
+    role.addEventListener('change', (e)=>{
+        if (role_value === "teacher"){
+            class_box.style.display = "none";
+        }
+        if (role_value === "student"){
+            class_box.style.display = "flex";
+        }
+    })
 });
