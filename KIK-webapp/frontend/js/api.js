99209c5cc3a06f77a85159414daf5b16400dbf28
xenoo_ch <gamesosmedacc@gmai.com>
2026-09-26 20:41:21 +0800

    first deployment

--- KIK-webapp/frontend/js/api.js
+++ KIK-webapp/frontend/js/api.js
@@ -1,6 +1,10 @@
+const url = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+? "http://127.0.0.1:5000"
+: "https://projectkik-production.up.railway.app";
+
 async function api_requests(endpoint, method, data) {
     try {
-        const response = await fetch(`http://127.0.0.1:5000${endpoint}`, {
+        const response = await fetch(`${url}${endpoint}`, {
             method: method,
             headers: { "content-type": "application/json", "bypass-tunnel-reminder": "true"},
             credentials : "include",
@@ -16,7 +20,7 @@ async function api_requests(endpoint, method, data) {
 
 async function api_get(endpoint, method) {
     try {
-        const response = await fetch(`http://127.0.0.1:5000${endpoint}`, {
+        const response = await fetch(`${url}${endpoint}`, {
             method : method,
             headers : { "content-type": "application/json"},
             credentials : "include"
