299ade4275e80d4462dfb72fe1b07817b52d1a75
xenoo_ch <gamesosmedacc@gmai.com>
2026-09-26 17:06:25 +0800

    pre-deployment

--- KIK-webapp/backend/utils/helper/get_from_database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, select, Column, Integer, String
-from utils.tools.database import base, engine, Session_local
-from utils.tools.users import users
-
-session = Session_local()
-def get_from_database(session_id):
-    data = session.scalars(select(users).where(users.id == session_id )).first()
-
-    return data
